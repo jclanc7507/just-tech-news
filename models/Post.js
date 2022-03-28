4d03078db6129727cd5ad20b1b5c7deebb24022d
Jeremiah <jclanc7507@gmail.com>
2022-03-28 16:54:30 -0400

    resolving conflict

---
+++ models/Post.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('sequelize');
+// 13.3.4
+// create our Post model
+class Post extends Model {}
+
+// create fields/columns for Post model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true, 
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+model.exports = Post;
